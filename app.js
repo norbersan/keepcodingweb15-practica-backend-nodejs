b49e49f62db11dd236684f533fd4d09341a03d58
norber_san <norberto.sanchezpuertas@aldautomotive.com>
2023-10-03 13:23:36 +0200

    minor changes

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,28 +9,28 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+var nodemon = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+nodemon.set('views', path.join(__dirname, 'views'));
+nodemon.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+nodemon.use(logger('dev'));
+nodemon.use(express.json());
+nodemon.use(express.urlencoded({ extended: false }));
+nodemon.use(cookieParser());
+nodemon.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+nodemon.use('/', indexRouter);
+nodemon.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+nodemon.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+nodemon.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +40,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = nodemon;
