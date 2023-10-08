24cedad4c650755f1c932e9b8fa84550589db477
norber_san <norberto.sanchezpuertas@aldautomotive.com>
2023-10-08 17:13:07 +0200

    documentation in /api-doc

--- routes/index.js
+++ routes/index.js
@@ -61,12 +61,32 @@ router.post('/', async (req, res, next) => {
   }
 });
 
+/**
+ * @openapi
+ * /api/articles:
+ *  post:
+ *   description: Return the list of tags
+ *   responses:
+ *    200:
+ *     description: returns JSON
+ */
+router.get('/tags', function(req, res, next) {
+  res.send('["work","lifestyle","motor","mobile"]');
+})
+
+/**
+ * @openapi
+ * /api/articles/ui:
+ *  get:
+ *   description: Simple ui to show all articles
+ *   responses:
+ *    200:
+ *     description: returns html page with all articles
+ */
 router.get('/ui', async function(req, res, next) {
 
   res.locals.title = "Articles list";
-  res.locals.articles = JSON.stringify(await Articles.lista({}));
-
-  console.log('Article list : ' + res.locals.articles)
+  res.locals.articles = await Articles.lista({});
 
   res.render('../views/index.ejs');
 });
